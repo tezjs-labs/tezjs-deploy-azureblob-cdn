ac0a0da8afe10d67f187ec88242ad6ea1a417a6f
kansara <darshil@EC2AMAZ-LVS4BNQ.temp.local>
2022-08-08 09:47:43 +0000

    project revert for new one

--- tez.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineTezConfig } from "@tezjs/vite"
-import { routeModule } from '@tezjs/route'
-export default defineTezConfig({
-    client:{
-        imports:['/@/assets/index.css']
-    },
-    modules:[routeModule]
-})
